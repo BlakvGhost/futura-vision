20aef100974824c568accad282506ddc61f87e06
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-03-17 12:33:50 +0100

    fix mail send from contact

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -15,55 +15,32 @@ import Alert from './Alert.vue';
         <div class="row row-col-2 my-3 fv-f-direc-col-sm">
           <div class="form-group col">
             <label for="yourName" class="form-text">Nom* : </label>
-            <input
-              type="text"
-              name="name"
-              id="yourFirstName"
-              placeholder="Entrer votre Nom"
-              class="form-control form-control-lg"
-              v-model="user.last_name"
-            />
+            <input type="text" name="name" id="yourFirstName" placeholder="Entrer votre Nom"
+              class="form-control form-control-lg" v-model="user.last_name" />
           </div>
           <div class="form-group col">
             <label for="yourLastName" class="form-text">Prenom* : </label>
-            <input
-              type="text"
-              name="surname"
-              id="yourLastName"
-              placeholder="Entrer votre Prenom"
-              class="form-control form-control-lg"
-              v-model="user.first_name"
-            />
+            <input type="text" name="surname" id="yourLastName" placeholder="Entrer votre Prenom"
+              class="form-control form-control-lg" v-model="user.first_name" />
           </div>
         </div>
         <div class="form-group my-2">
+          <label for="youremail" class="form-text">Sujet* : </label>
+          <input type="text" name="subject" id="subject" placeholder="Entrer le sujet de votre mail"
+            class="form-control form-control-lg" v-model="user.subject" />
+        </div>
+        <div class="form-group my-2">
           <label for="youremail" class="form-text">Email* : </label>
-          <input
-            type="email"
-            name="email"
-            id="youremail"
-            placeholder="Entrer votre Email pour etre repondu"
-            class="form-control form-control-lg"
-            v-model="user.email"
-          />
+          <input type="email" name="email" id="youremail" placeholder="Entrer votre Email pour etre repondu"
+            class="form-control form-control-lg" v-model="user.email" />
         </div>
         <div class="form-group">
           <label for="message" class="form-text">Message* : </label>
-          <textarea
-            name="message"
-            id="message"
-            placeholder="Entrer votre Message"
-            class="form-control form-control-lg"
-            cols="40"
-            rows="8"
-            v-model="message"
-          ></textarea>
+          <textarea name="message" id="message" placeholder="Entrer votre Message" class="form-control form-control-lg"
+            cols="40" rows="8" v-model="message"></textarea>
         </div>
         <div class="text-center my-2">
-          <button
-            type="submit"
-            class="btn btn-success fv-w-50-lg fv-w-75-sm m-auto"
-          >
+          <button type="submit" class="btn btn-success fv-w-50-lg fv-w-75-sm m-auto">
             ENVOYER
           </button>
         </div>
@@ -86,19 +63,19 @@ export default {
     insert: function (data) {
       this.$Progress.start();
       this.$http.post("contact", data, this.$token()).then((response) => {
-          this.notifs = response.data.message;
-          this.state = response.data.success
-          this.$toastr("Nous-Contactez", this.notifs, "success");
-          this.$Progress.finish();
-        }).catch(error => {
-          this.notifs = error.response.data.message;
-          this.state = error.response.data.success;
-          this.$toastr("POST", this.$BadFieldsErrorMessage, "error");
-          this.$Progress.fail();
-        })
+        this.notifs = response.data.message;
+        this.state = response.data.success
+        this.$toastr("Nous-Contactez", this.notifs, "success");
+        this.$Progress.finish();
+      }).catch(error => {
+        this.notifs = error.response.data.message;
+        this.state = error.response.data.success;
+        this.$toastr("POST", this.$BadFieldsErrorMessage, "error");
+        this.$Progress.fail();
+      })
     },
     form: function () {
-      $('html, body').animate({scrollTop: 0},100 );
+      $('html, body').animate({ scrollTop: 0 }, 100);
       this.user['message'] = this.message;
       if (this.user.first_name && this.user.last_name && this.user.email && this.user.message) {
         this.insert(this.user);
