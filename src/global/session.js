dec47be4ad1ee084e48628b06dbeeece6a7650c8
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-03-19 14:05:35 +0100

    debugging

--- src/global/session.js
+++ src/global/session.js
@@ -27,99 +27,102 @@ export default function session(self) {
         let auth = JSON.parse(sessionStorage.getItem(AUTH_SESSION_NAME)) ?? {};
 
         if (!auth.token) {
-            const { data } = http.post('login', {
+            http.post('login', {
                 email: DEFAULT_USER_EMAIL,
                 password: DEFAULT_USER_PASSWORD
+            }).then(res => {
+                sessionStorage.setItem(AUTH_SESSION_NAME, JSON.stringify(res.data));
+                auth = res.data;
             });
-            sessionStorage.setItem(AUTH_SESSION_NAME, JSON.stringify(data.data));
-            auth = data.data;
         } else if (auth.role !== 'get') {
-            try {
-                const { data } = http.get('current-user', getToken());
-                sessionStorage.setItem(AUTH_SESSION_NAME, JSON.stringify(data.data));
-                auth = data.data;
-            } catch (error) {
-                sessionStorage.removeItem(AUTH_SESSION_NAME);
-            }
+            http.get('current-user', getToken())
+                .then(res => {
+                    sessionStorage.setItem(AUTH_SESSION_NAME, JSON.stringify(res.data));
+                    auth = res.data;
+                })
+                .catch(error => {
+                    sessionStorage.removeItem(AUTH_SESSION_NAME);
+                })
         }
-        return auth.role !== 'get' ? auth : false;
-    };
+    }
+    return auth.role !== 'get' ? auth : false;
+};
 
-    const logout = () => {
-        sessionStorage.removeItem(AUTH_SESSION_NAME);
-        getCurrentUser();
-    };
+const logout = () => {
+    sessionStorage.removeItem(AUTH_SESSION_NAME);
+    getCurrentUser();
+};
 
-    const sanitize = (text) => {
-        return text.replace(/<[^>]*>/g, '').replace(/&(nbsp|#160|ensp|emsp|thinsp);/g, ' ');
-    };
+const sanitize = (text) => {
+    return text.replace(/<[^>]*>/g, '').replace(/&(nbsp|#160|ensp|emsp|thinsp);/g, ' ');
+};
 
-    /**
- * Convertit une chaîne de caractères en un format valide pour une URL.
- * 
- * @param {string} title - La chaîne de caractères à convertir.
- * @returns {string} La chaîne de caractères convertie.
- */
-    const urlify = (title) => {
-        const sanitizedTitle = typeof title === 'string' ? title : '';
-        const urlifiedTitle = sanitizedTitle
-            .normalize("NFD")
-            .replace(/[\u0300-\u036f]/g, "")
-            .replace(/[^\w\s-]/g, '')
-            .replace(/[\s]+/g, '-')
-            .toLowerCase();
+/**
+* Convertit une chaîne de caractères en un format valide pour une URL.
+* 
+* @param {string} title - La chaîne de caractères à convertir.
+* @returns {string} La chaîne de caractères convertie.
+*/
+const urlify = (title) => {
+    const sanitizedTitle = typeof title === 'string' ? title : '';
+    const urlifiedTitle = sanitizedTitle
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .replace(/[^\w\s-]/g, '')
+        .replace(/[\s]+/g, '-')
+        .toLowerCase();
 
-        return urlifiedTitle;
-    };
+    return urlifiedTitle;
+};
 
-    const reduceText = (data, length) => {
-        const text = sanitize(data ?? '');
-        return text.slice(0, length);
-    };
+const reduceText = (data, length) => {
+    const text = sanitize(data ?? '');
+    return text.slice(0, length);
+};
 
-    const reverseData = (data) => {
-        return data;
-    };
+const reverseData = (data) => {
+    return data;
+};
 
-    const avoidUndefined = (object, key) => {
-        try {
-            return sanitize(object[key]) ?? ' ';
-        } catch (error) {
-            return ' ';
-        }
-    };
+const avoidUndefined = (object, key) => {
+    try {
+        return sanitize(object[key]) ?? ' ';
+    } catch (error) {
+        return ' ';
+    }
+};
 
-    const avoidUndefinedArray = (object, key) => {
-        try {
-            return object[key] ?? [];
-        } catch (error) {
-            return [];
-        }
-    };
+const avoidUndefinedArray = (object, key) => {
+    try {
+        return object[key] ?? [];
+    } catch (error) {
+        return [];
+    }
+};
 
-    const loadImage = (link) => {
-        return STORAGE_URL + (link || 'default.png');
-    };
+const loadImage = (link) => {
+    return STORAGE_URL + (link || 'default.png');
+};
 
-    const loadImageFV = (link) => {
-        return STORAGE_URL + (link || 'logo.jpg');
-    };
+const loadImageFV = (link) => {
+    return STORAGE_URL + (link || 'logo.jpg');
+};
 
-    self.$defaultTitle = DEFAULT_TITLE;
-    self.$base_url = BASE_URL;
-    self.$api_url = API_URL;
-    self.$storage_url = STORAGE_URL;
-    self.$authSessionName = AUTH_SESSION_NAME;
-    self.$token = getToken;
-    self.$http = http;
-    self.$currentUser = getCurrentUser;
-    self.$logout = logout;
-    self.$url = urlify;
-    self.$sanitaze = sanitize;
-    self.$reduce = reduceText;
-    self.$reverse = reverseData;
-    self.$avoid = avoidUndefined;
-    self.$avoidr = avoidUndefinedArray;
-    self.$loadImage = loadImage;
-    self.$loadImageFV = loadImageFV;
+self.$defaultTitle = DEFAULT_TITLE;
+self.$base_url = BASE_URL;
+self.$api_url = API_URL;
+self.$storage_url = STORAGE_URL;
+self.$authSessionName = AUTH_SESSION_NAME;
+self.$token = getToken;
+self.$http = http;
+self.$currentUser = getCurrentUser;
+self.$logout = logout;
+self.$url = urlify;
+self.$sanitaze = sanitize;
+self.$reduce = reduceText;
+self.$reverse = reverseData;
+self.$avoid = avoidUndefined;
+self.$avoidr = avoidUndefinedArray;
+self.$loadImage = loadImage;
+self.$loadImageFV = loadImageFV;
 }
