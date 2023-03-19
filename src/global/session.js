af635825adfb6e23247ad76e602dd028faa4e342
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-03-19 13:43:50 +0100

    debugging

--- src/global/session.js
+++ src/global/session.js
@@ -35,15 +35,14 @@ export default function session(self) {
             auth = data.data;
         } else if (auth.role !== 'get') {
 
-            try {
-                const { data } = await http.get('current-user', getToken());
-                data.then(res => {
+            await http.get('current-user', getToken())
+                .then(res => {
                     sessionStorage.setItem(AUTH_SESSION_NAME, JSON.stringify(res.data));
                     auth = res.data;
                 })
-            } catch (error) {
+                .catch(error => {
                 sessionStorage.removeItem(AUTH_SESSION_NAME);
-            }
+            })
         }
 
         return auth.role !== 'get' ? auth : false;
